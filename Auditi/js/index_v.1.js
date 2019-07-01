10a1a2dadc86043f56ac07b1123affaf2fc73e55
pilotvitalik <itvkip@yandex.ru>
2019-07-01 21:55:56 +0300

    Изменения в анимации баннера

---
+++ Auditi/js/index_v.1.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('#womanBag').css('display','none');
+  $('#whiteTees').css('display','none');
+  $('#darkTees').css('display','none');
+  $('#levis').css('display','none');
+  $('.banner ul li:first').css('background','#79d8cc');
+});
