6e49aaaf6bacd80027182cdb20c7ad94d6caf1bf
pilotvitalik <itvkip@yandex.ru>
2019-07-05 19:23:38 +0300

    Создание слайдера для раздела категории на jQuery

--- Auditi/js/index.js
+++ Auditi/js/index.js
@@ -3,7 +3,11 @@ $(document).ready(function(){
 	a.css('width','305px');
 	$('.banner div:first-child .item:nth-child(2) form p').css('top','110px');
 	$('.banner div:first-child .item:nth-child(4) form p').css('top','110px');
-
+    $('.categories li:nth-child(4) div h5').css('width','163px');
+    $('.categories li:nth-child(4) div button').css('margin','120px 0 0 -163px');
+    $('.categories li:nth-child(2) div img').css('margin','26.5px 0 0 40px');
+    $('.categories li:nth-child(3) div img').css('margin','26.5px 0 0 60px');
+    
 	var owl = $('.owl-carousel');
               owl.owlCarousel({
                 items: 1,
@@ -13,4 +17,14 @@ $(document).ready(function(){
                 autoplayTimeout: 7000,
                 autoplayHoverPause: true
               });
+
+  $("#flexiselDemo3").flexisel({
+        visibleItems: 3,
+        itemsToScroll: 1,         
+        autoPlay: {
+            enable: true,
+            interval: 8000,
+            pauseOnHover: true
+        }        
+    });
 });
